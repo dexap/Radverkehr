f4c815d4898a0805574fd303f12d7b34b9c30460
Benjamin Lassmann <benjamin.lassmann@student.htw-berlin.de>
2022-09-22 13:43:18 +0200

    Initial Commit

---
+++ Radverkehr/RadverkehrApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadverkehrApp.swift
+//  Radverkehr
+//
+//  Created by Benjamin Lassmann on 22.09.22.
+//
+
+import SwiftUI
+
+@main
+struct RadverkehrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
